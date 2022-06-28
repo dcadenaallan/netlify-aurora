985bc0d5cebbf6bb7a89066f0d6b47e30a22daad
Danilo Cadena <danilo.cadena@allansolutions.com>
2022-06-28 09:59:25 -0500

    feat: added prisma

--- functions/normal-test/src/app.routes.ts
+++ functions/normal-test/src/app.routes.ts
@@ -4,9 +4,14 @@ import {PrismaClient} from "@prisma/client";
 export class AppRoutes {
   static create(router: Router) {
     router.get('/', async (req: Request, res: Response) => {
-      const prismaClient = new PrismaClient()
-      const data = await prismaClient.user.findMany()
-      res.json({data})
+      try{
+        const prismaClient = new PrismaClient()
+        const data = await prismaClient.user.findMany()
+        res.json({data})
+      }catch (err) {
+        console.error(err)
+        res.status(400).json({message: err.message})
+      }
     })
   }
 }
