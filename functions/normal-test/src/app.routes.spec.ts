d63f0e2634951375c684681e97b082fb13a1f174
Danilo Cadena <danilo.cadena@allansolutions.com>
2022-07-01 20:29:32 -0500

    feat(Jest): test container

--- functions/normal-test/src/app.routes.spec.ts
+++ functions/normal-test/src/app.routes.spec.ts
@@ -3,8 +3,11 @@ import {PrismaClient} from "@prisma/client";
 describe('AppRoutes', () => {
   let prisma
 
-  beforeEach(() => {
+  beforeEach(async () => {
     prisma = new PrismaClient()
+    await prisma.$executeRaw`insert into User (id, createdAt, email, name)
+                             values (1, '2022-06-28 15:24:10.491', 'test1@test.com', 'Test One'),
+                                    (2, '2022-06-28 15:47:33.851', 'test2@test.com', 'Test Two');`
   })
   it('Test1', async () => {
     const users = await prisma.user.findMany()
